127b6303f924174b7b4ef940065c4f1a09e3c774
inventsekar <inventsekarinfo@gmail.com>
2018-03-05 15:08:46 +0530

    wc thru python
    wc thru python

---
+++ linux-wc-thru-python.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# inventsekar 5th march 2018
+# python implementation of Linux tool "wc"
+
+line1 = raw_input("Please enter line1:");
+line2 = "A quite bubble floating on a sea of noise. - The God of Small Things - Arundhati Roy";
+line3 = "Hello, How are you";
+
+
+#-------------------------------------#
+# Finding number of words: wc -w
+#-------------------------------------#
+
+print "Finding numer of words: wc -w\n";
+print "---------------------------------\n";
+
+#$nWords=0;
+
+#while ($line1 =~ /\b\w+\b/g){ $nWords++; }
+print "line1 is:", line1;
+#print "\$line1 contains $nWords words\n";
