1203d714b7ef6f2b00dbad614ff8a35a58cbd86f
Sekar Sundaram <inventsekarinfo@gmail.com>
2018-03-06 19:04:37 +0530

    1

--- python-syntax-sheet.py
+++ python-syntax-sheet.py
@@ -14,6 +14,9 @@
 #print
 #-------------------------------------
 
+# In Python 2, raw_input() returns a string, and input() tries to run the input as a Python expression.
+
+# for Python 3.x
 # The difference is that raw_input() does not exist in Python 3.x, while input() does. 
 # Actually, the old raw_input() has been renamed to input(), and the old input() is gone, 
 # but can easily be simulated by using eval(input()). 
