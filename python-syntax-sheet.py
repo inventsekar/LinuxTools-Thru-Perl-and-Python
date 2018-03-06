ba7a1d3e02cdf9defdad5c3135b749cb876ee001
Sekar Sundaram <inventsekarinfo@gmail.com>
2018-03-06 18:55:30 +0530

    test

--- python-syntax-sheet.py
+++ python-syntax-sheet.py
@@ -1,9 +1,28 @@
 # 06th march 2018 invent sekar
 # python syntax learnings
 
+
+
+#-------------------------------------
+#comment, single line, multiline
+#-------------------------------------
+# a hash creates a single line comment
+# there is no multiline comment on python. just use # on every line.
+
+
 #-------------------------------------
 #print
 #-------------------------------------
+
+# The difference is that raw_input() does not exist in Python 3.x, while input() does. 
+# Actually, the old raw_input() has been renamed to input(), and the old input() is gone, 
+# but can easily be simulated by using eval(input()). 
+# (Remember that eval() is evil, so if try to use safer ways of parsing your input if possible.)
+# from stock overflow user Sven Marnach
+
+
+
+
 
 
 
